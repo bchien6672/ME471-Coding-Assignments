abbf30567dde408c295c467ca349fad2a9c12b9c
Brian Chien <bchien6672@gmail.com>
2018-03-28 02:56:45 -0500

    Add files via upload
    Finished up to plotting

--- Programming Assignment 2.py
+++ Programming Assignment 2.py
@@ -14,10 +14,28 @@ from sympy import simplify
 #problem conditions (Assume consistent units)
 f = 2
 A = 1
-E = 10e4
+E = pow(10, 4)
 P = 10
 
 """ Preprocess Functions"""
+def solve_function(global_K, force):
+    #imposes BC
+    before_constrain = global_K.tolist()
+    for item in before_constrain:
+        item[0] = 0
+    force = np.delete(force, 0, 0)
+    after_constrain = before_constrain
+
+    after_constrain.pop(0)
+    for item in after_constrain:
+        item.pop(0)
+    after_constrain = np.matrix(before_constrain)
+
+    d = np.linalg.inv(after_constrain) * force
+    d = np.insert(d, 0, [0], axis=0)
+
+    return d
+
 def getuppermatrix(gen_stiffness, local_dim, global_dim):
     upper_corner = []
     for i in range(0, global_dim):
@@ -91,16 +109,24 @@ def assemble_globalstiffness(global_dimension, global_stiffness, element_stiffne
 
 def preprocess(elements):
     nodes = elements + 1
+    # develop general equation
+    element_length = 1 / float(elements)
+    a_e = A * E #serves as main coefficient
+
     global_stiffness = np.zeros((nodes, nodes))
     force_vector = np.zeros((nodes, 1))
+
+    #implement internal beam force
+    int_forcevector = np.zeros((nodes, 1))
+    int_forcevector[0][0] = element_length
+    for i in range(1, len(int_forcevector)):
+        int_forcevector[i][0] = element_length * f
 
     #implement point load
     load_vector = np.zeros((nodes, 1))
     load_vector[nodes - 1, 0] = P
 
-    # develop general equation
-    element_length = 1 / float(elements)
-    a_e = A * E #serves as main coefficient
+    force_vector = int_forcevector + load_vector
 
     #create element stiffness matrix
     K_11 = 1/float(element_length)
@@ -113,10 +139,95 @@ def preprocess(elements):
     global_K = assemble_globalstiffness(nodes, global_stiffness, element_stiffness)
     global_K = np.multiply(a_e, global_K) #AE* K
 
-    d_params = None
+    d_params = solve_function(global_K, force_vector)
     return d_params
 
+""" Post processing """
+def postprocess(d_param, element_num):
+    node_loc = []
+    strain_list = []
+    stress_list = []
 
+    element_length = 1 / float(element_num)
+
+    for i in range(0, element_num + 1):
+        node_loc.append(i * element_length)
+
+    #calculating strain
+    for i in range(0, element_num):
+        strain = (2/element_length) * ((d_param[i + 1] - d_param[i])/2)
+        strain_list.append(strain)
+
+    for item in strain_list:
+        ind = strain_list.index(item)
+        item = item.tolist()
+        val = item[0][0]
+        strain_list[ind] = val
+
+    #calculating stress
+    for i in range(0, element_num):
+        stress = (E/element_length) * (d_param[i + 1] - d_param[i])
+        stress_list.append(stress)
+
+    stress_list.append(stress_list[-1])
+
+    reformatted_stresslist = []
+
+    for item in stress_list:
+        item = item.tolist()
+        val = item[0][0]
+        reformatted_stresslist.append(val)
+
+        stress_list = reformatted_stresslist
+
+    x_midpt, mid_stress = calc_midptstress(element_length, node_loc)
+
+    plot_stress(node_loc, stress_list, x_midpt, mid_stress)
+
+def calc_midptstress(element_length, node_loc):
+    half_length = element_length / 2
+    updated_nodeloc = []
+    midpt_stress = []
+
+    for i in node_loc:
+        if i != node_loc[-1]:
+            point = i + half_length
+            updated_nodeloc.append(point)
+
+    for i in updated_nodeloc:
+        stress = (2/A) * (6 - i)
+        midpt_stress.append(stress)
+
+    return updated_nodeloc, midpt_stress
+
+def calc_qtrptstress(element_length, node_loc):
+    qtr_length = element_length / 4
+    updated_nodeloc = []
+    qtrpt_stress = []
+
+    for i in node_loc:
+        if i != node_loc[-1]:
+            point = i + qtr_length
+            updated_nodeloc.append(point)
+
+    for i in updated_nodeloc:
+        stress = (2/A) * (6 - i)
+        qtrpt_stress.append(stress)
+
+    return updated_nodeloc, qtrpt_stress
+
+def plot_stress(x1, y1, x2, y2):
+    plt.figure()
+    #duplicate middle nodes
+    val_duplicate =
+    print x1
+    #plt.plot(x1, y1)
+    plt.scatter(x2, y2, c = 'g')
+    plt.show()
+    return
+
+def calc_error():
+    return
 
 def main():
     #Introduction to program
@@ -124,6 +235,6 @@ def main():
     elements_num = int(elements_num)
     print "You have selected " + str(elements_num) + " elements."
     d_params = preprocess(elements_num)
-    #postprocess(d_params, elements_num)
+    postprocess(d_params, elements_num)
 
 if __name__ == "__main__": main()
