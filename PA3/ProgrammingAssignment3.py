45261e80dc0ef1fc8380c2053a4a6fbae7bb1c82
Brian Chien <bchien6672@gmail.com>
2018-04-15 14:59:19 -0500

    Finished up initial text parser

--- PA3/ProgrammingAssignment3.py
+++ PA3/ProgrammingAssignment3.py
@@ -20,7 +20,6 @@ def text_parser(txt):
     section = []
 
     for line in txt:
-        print line
         section.append(line)
         if line == '':
             parsed_txt.append(section)
@@ -49,7 +48,6 @@ def create_inputdec(txt): #takes in text file as series of lists
     param_dict['Problem Param.']['Loaded Nodes'] = nodal_info[2]
     param_dict['Problem Param.']['Constained Nodes'] = nodal_info[3]
 
-    #Assume there are only four coordinates
     element_coords = parsed_txt[1]
     for pt in element_coords:
         pt_list = pt.split()
@@ -58,9 +56,42 @@ def create_inputdec(txt): #takes in text file as series of lists
         param_dict['Node Data'][node_val]['X'] = pt_list[1]
         param_dict['Node Data'][node_val]['Y'] = pt_list[2]
 
-    
+    element_data = parsed_txt[2]
+    for element in element_data:
+        element_component = element.split()
+        element_num = 'Element ' + str(element_component[0])
+        param_dict['Element Data'][element_num] = {}
+        param_dict['Element Data'][element_num]['First Node'] = element_component[1]
+        param_dict['Element Data'][element_num]['Second Node'] = element_component[2]
+        param_dict['Element Data'][element_num]['Cross-Section Area'] = element_component[3]
+        param_dict['Element Data'][element_num]['Y-Modulus'] = element_component[4]
 
-    print param_dict
+    force_data = parsed_txt[3]
+    for force in force_data:
+        force_point = force.split()
+        force_at_node = 'Node ' + str(force_point[0])
+        param_dict['Force Data'][force_at_node] = {}
+        param_dict['Force Data'][force_at_node]['X-Dir'] = force_point[1] #lb
+        param_dict['Force Data'][force_at_node]['Y-Dir'] = force_point[2]
+
+    constraint_data = parsed_txt[4]
+    for constraint in constraint_data:
+        constraint_node = constraint.split()
+        node_num = 'Node ' + str(constraint_node[0])
+        param_dict['Constraint Data'][node_num] = {}
+
+        x_constrainbool = constraint_node[1]
+        y_constrainbool = constraint_node[2]
+
+        if x_constrainbool != 1:
+            param_dict['Constraint Data'][node_num]['X-disp'] = constraint_node[3]
+        else:
+            param_dict['Constraint Data'][node_num]['X-disp'] = 0
+
+        if y_constrainbool != 1:
+            param_dict['Constraint Data'][node_num]['Y-disp'] = constraint_node[4]
+        else:
+            param_dict['Constraint Data'][node_num]['Y-disp'] = 0
 
     return param_dict
 
